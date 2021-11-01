d4656daec7910bf3b320ffd05db57fefd0341847
Cenkaifeng <cenkaifeng@foxmail.com>
2021-11-01 20:22:38 +0800

    mod: week8 component Carousel

--- AnewFrontEnd/week8_components/jsx/framework.js
+++ AnewFrontEnd/week8_components/jsx/framework.js
@@ -2,9 +2,9 @@ export function createElement(type, attributes, ...children) {
   let element;
 
   if (typeof type === "string") {
-    element = document.createElement(type);
-  } else {
     element = new ElementWrapper(type);
+  } else {
+    element = new type();
   }
   for (let name in attributes) {
     element.setAttribute(name, attributes[name]);
@@ -15,34 +15,29 @@ export function createElement(type, attributes, ...children) {
     }
     element.appendChild(child);
   }
-  return;
+  return element;
+}
+export class Component {
+  constructor(type) {}
+
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value);
+  }
+  appendChild(child) {
+    child.appendChild(this.root);
+  }
+  mountTo(parent) {
+    parent.appendChild(this.root);
+  }
 }
 
-class ElementWrapper {
+class ElementWrapper extends Component {
   constructor(type) {
     this.root = document.createElement(type);
   }
-  setAttribute(name, value) {
-    this.root.setAttribute(name, value);
-  }
-  appendChild(child) {
-    this.root.appendChild(child);
-  }
-  mountTo(parent) {
-    parent.appendChild(this.root);
-  }
 }
-class TextWrapper {
+class TextWrapper extends Component {
   constructor(content) {
     this.root = document.createTextNode(content);
-  }
-  setAttribute(name, value) {
-    this.root.setAttribute(name, value);
-  }
-  appendChild(child) {
-    this.root.appendChild(child);
-  }
-  mountTo(parent) {
-    parent.appendChild(this.root);
   }
 }
--- AnewFrontEnd/week8_components/jsx/main.js
+++ AnewFrontEnd/week8_components/jsx/main.js
@@ -1,19 +1,70 @@
-class Carousel {
+import { Component, createElement } from "./framework";
+
+class Carousel extends Component {
   constructor() {
-    this.root = document.createElement("div");
+    super();
+    this.attributes = Object.create(null);
   }
   setAttribute(name, value) {
-    this.root.setAttribute(name, value);
+    this.attributes[name] = value;
   }
-  appendChild(child) {
-    child.mountTo(this.root);
+  render() {
+    console.log(this.attributes.src);
+    this.root = document.createElement("div");
+    this.root.classList.add("carousel");
+    for (let record of this.attributes.src) {
+      let child = document.createElement("div");
+      child.style.backgroundImage = `url('${record}')`;
+      child.src = record;
+      this.root.appendChild(child);
+    }
+
+    this.root.addEventListener("mousedown", event => {
+      console.log("mousedown");
+
+      let move = event => {
+        console.log("mousemove");
+      };
+
+      let up = event => {
+        console.log("mouseup");
+        document.removeEventListener("mousemove", move);
+        document.removeEventListener("mouseup", move);
+      };
+
+      document.addEventListener("mousemove", move);
+      document.addEventListener("mouseup", up);
+    });
+
+    // 下方是自动播放逻辑
+    /*
+    let currentIndex = 0;
+    setInterval(() => {
+      // 下面这个逻辑其实就是把轮播标识抽象成为，当前、下一个，两个标识
+      let children = this.root.children;
+      let nextIndex = (currentIndex + 1) % children.length;
+
+      let current = children[currentIndex];
+      let next = children[nextIndex];
+
+      next.style.transition = "none";
+      next.style.transform = `translateX(-${100 - nextIndex * 100}%)`;
+
+      setTimeout(() => {
+        next.style.transition = "";
+        current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
+        next.style.transform = `translateX(${-nextIndex * 100}%)`;
+        currentIndex = nextIndex;
+      }, 16);
+      // 此处使用 requestAnimationFrame 会有问题，需要使用两次
+    }, 3000);
+    */
+    return this.root;
   }
   mountTo(parent) {
-    parent.appendChild(this.root);
+    parent.appendChild(this.render());
   }
 }
-
-let a = <Div id="a">Hello !</Div>;
 
 // document.body.appendChild(a);
 let d = [
@@ -22,5 +73,5 @@ let d = [
   "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
   "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg]",
 ];
-
+let a = <Carousel src={d} />;
 a.mountTo(document.body);
