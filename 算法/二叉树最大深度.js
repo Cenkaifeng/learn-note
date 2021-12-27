6a25ae6dfc85579381c693d8cd477912fdf551be
Cenkaifeng <cenkaifeng@foxmail.com>
2021-12-27 20:04:05 +0800

    add: tree depth BFS

--- 算法/二叉树最大深度.js
+++ 算法/二叉树最大深度.js
@@ -16,4 +16,37 @@ function maxDepth(root) {
   }
 }
 
-// TODO: BFS 解法
+// BFS 解法 层序遍历
+
+/**
+ * 1. root === null return 0
+ * 2. 初始化：queue = [root] counter = 0;
+ * 3. while 循环遍历 [] break
+ *  1. 初始化空列表tmp储存下一层节点；
+ *  2. 遍历队列各节点，将其子节点都加入 tmp
+ *  3. 更新队列 queue = tmp, 将下一层节点赋值给queue
+ *  4. 统计层数 res ++
+ */
+
+function maxDepth(root) {
+  if (root === null) {
+    return 0;
+  }
+  let queue = [root];
+  let counter = 0;
+
+  while (queue.length) {
+    let tmp = [];
+
+    for (let item of queue) {
+      item.left && tmp.push(item.left);
+      item.right && tmp.push(item.right);
+    }
+
+    queue = tmp;
+
+    counter++;
+  }
+
+  return counter;
+}
