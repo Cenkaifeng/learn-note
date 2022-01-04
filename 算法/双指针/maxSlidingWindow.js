0a0ddd63be4ce0c6d3830f493ef4b1df8f7d87ee
Cenkaifeng <cenkaifeng@foxmail.com>
2022-01-04 08:33:52 +0800

    mod: maxSlidingWindow tips

--- 算法/双指针/maxSlidingWindow.js
+++ 算法/双指针/maxSlidingWindow.js
@@ -2,7 +2,7 @@
 
 /**
  * @param {number[]} nums
- * @param {number} k
+ * @param {number} k 窗口大小值
  * @return {number[]}
  */
 function maxSlidingWindow(nums, k) {
@@ -10,14 +10,16 @@ function maxSlidingWindow(nums, k) {
   let wr = wl + k - 1;
   let res = [];
   while (wr <= nums.length - 1) {
-    let focus = [];
+    let focus = []; // 处理窗口内数据的临时数组
     if (wr > nums.length - 1) {
+      // 右侧窗口达到了 nums 最大值终止
       break;
     } // 窗口终止
     for (let i = wl; i <= wr; i++) {
       focus.push(nums[i]);
       if (i === wr) {
-        res.push(focus.sort((a, b) => a - b).pop());
+        // 窗口内数字的最后一个
+        res.push(focus.sort((a, b) => a - b).pop()); // 每次把focus的最大值通过排序后放进 res
       }
     }
     wl++; // 窗口移动
