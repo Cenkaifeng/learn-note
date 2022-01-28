2afcf4bf81649d1c093388d5f541a6b555f603fd
Cenkaifeng <cenkaifeng@foxmail.com>
2022-01-28 18:37:21 +0800

    mod: 算法

--- 算法/处理特定的数组转树结构.js
+++ 算法/处理特定的数组转树结构.js
@@ -5,3 +5,26 @@
  *
  * 要求尽可能少的复杂度实现将当前结构转化成树结构
  */
+
+// 解 1
+
+function convert(list) {
+  const res = [];
+  const map = list.reduce(
+    (res, v) => ((res[v.id] = v), (v.children = []), res),
+    {}
+  ); // 初始化对象
+  // map 把所有节点挂上 chidren
+  for (const item of list) {
+    if (item.pid === 0) {
+      res.push(item);
+      continue; // 根节点处理
+    }
+
+    if (item.pid in map) {
+      const parent = map[item.pid];
+      parent.children = parent.children || []; // 置空
+      parent.children.push(item); // 推入节点
+    }
+  }
+}
