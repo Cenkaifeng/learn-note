3dc32850c33b2f68d7fe488e5d0296689828619c
Cenkaifeng <cenkaifeng@foxmail.com>
2022-02-19 22:57:47 +0800

    mod: browser object modal api and fetch xhr

--- fe_class/浏览器相关/ajax-demo.js
+++ fe_class/浏览器相关/ajax-demo.js
@@ -1,6 +1,6 @@
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'http://domain/serivce');
+xhr.open("GET", "http://domain/serivce");
 
 xhr.onreadystatechange = function () {
   if (xhr.readyState !== 4) {
@@ -10,85 +10,91 @@ xhr.onreadystatechange = function () {
   if (xhr.status === 200) {
     console.log(xhr.responseText);
   } else {
-    console.error(`HTTP error, status=${xhr.status}, errorText=${xhr.statusText}`);
+    console.error(
+      `HTTP error, status=${xhr.status}, errorText=${xhr.statusText}`
+    );
   }
-}
+};
 xhr.timeout = 3000;
 xhr.ontimeout = () => {
-  console.log(`当前请求超时啦！`)
-}
+  console.log(`当前请求超时啦！`);
+};
 
 xhr.upload.onprogress = p => {
-  const percent = Math.round((p.loaded / p.total) * 100) + '%';
-}
+  const percent = Math.round((p.loaded / p.total) * 100) + "%";
+};
 
 xhr.send(); // 注意：请求发送前一定要建立对状态的监听
 
-
-fetch('http://domain/service', {
-  method: 'GET',
-  credentials: 'same-origin' // 限制同域请求传cookie
-}).then(response => {
-  if (response.ok) {
-    // 请求成功
-    return response.json();
-  }
-
-  throw new Error('http error');
-}).then(json => {
-  console.log(json);
-}).catch(error => {
-  // 整体统一的错误管理 + fetch 本身的错误
+fetch("http://domain/service", {
+  method: "GET",
+  credentials: "same-origin", // 限制同域请求传cookie
 })
+  .then(response => {
+    if (response.ok) {
+      // 请求成功
+      return response.json();
+    }
+
+    throw new Error("http error");
+  })
+  .then(json => {
+    console.log(json);
+  })
+  .catch(error => {
+    // 整体统一的错误管理 + fetch 本身的错误
+  });
 
 //fetch 如何设置超时？ fetch本身不支持超时如何封装？
 
 function fetchTimeout(url, init, timeout = 3000) {
+  // 如果超时 fetchTimeout 完了就会触发 reject catch了
   return new Promise((resolve, reject) => {
+    // 在promise内很方便的改变它的状态，利用pendding状态改变不可逆的特性
     fetch(url, init).then(resolve).catch(reject);
     setTimeout(reject, timeout);
-  })
+  });
 }
-
-
 
 // 课后小作业：尝试封装一个通用的异步函数的超时逻辑
 
 function timeoutFn(fn, timeout) {
-  if (typeof fn !== 'function') {
-    throw new Error('入参必须为异步函数')
+  if (typeof fn !== "function") {
+    throw new Error("入参必须为异步函数");
   }
   let timer;
   return new Promise((resolve, reject) => {
-    resolve(fn())
+    resolve(fn());
     // 调用fn()
 
     timer = setTimeout(() => {
-      reject('timeout')
-    }, timeout)
-  })
+      reject("timeout");
+    }, timeout);
+  });
 }
-
 
 // 如何中断一个fetch?
 
 const controller = new AbortController();
 
-fetch('http://domain/service', {
-  method: 'GET',
-  credentials: 'same-origin',
-  signal: controller.signal
-}).then(response => {
-  if (response.ok) {
-    // 请求成功
-    return response.json();
-  }
-
-  throw new Error('http error');
-}).then(json => {
-  console.log(json);
-}).catch(error => {
-  // 整体统一的错误管理 + fetch 本身的错误
+fetch("http://domain/service", {
+  method: "GET",
+  credentials: "same-origin",
+  signal: controller.signal,
 })
+  .then(response => {
+    if (response.ok) {
+      // 请求成功
+      return response.json();
+    }
+
+    throw new Error("http error");
+  })
+  .then(json => {
+    console.log(json);
+  })
+  .catch(error => {
+    // 整体统一的错误管理 + fetch 本身的错误
+  });
 
 controller.abort();
--- fe_class/浏览器相关/ajax.ts
+++ fe_class/浏览器相关/ajax.ts
@@ -1,72 +1,77 @@
 interface IOptions {
   url: string;
-  type ? : 'GET' | 'POST';
+  type?: "GET" | "POST";
   data: any;
-  timeout ? : number;
+  timeout?: number;
 }
 
-function formatUrl(object) {
-    // a=xxx&b=xxx; querystring
-    let dataArr = [];
-    for (let key in object) {
-        dataArr.push(`${key}=${encodeURIComponent(object[key])}`);// 参数可能带url 中间会有一些解码问题
-    }
+function formatUrl(json) {
+  let object = { ...json }; // 浅拷贝一下，防止产生副作用修改引用类型的参数
+  // a=xxx&b=xxx; querystring
+  let dataArr = [];
+  for (let key in object) {
+    dataArr.push(`${key}=${encodeURIComponent(object[key])}`); // 参数可能带url 中间会有一些解码问题
+  }
 }
 
-export function ajax(options: IOptions = {
-  type: 'GET',
-  data: {},
-  timeout: 3000,
-  url: ''
-}) {
+export function ajax(
+  options: IOptions = {
+    type: "GET",
+    data: {},
+    timeout: 3000,
+    url: "",
+  }
+) {
   return new Promise((resolve, reject) => {
-    if (!options.url) { // 防止对方 ajax({} as any) 强制绕过类型检查
+    if (!options.url) {
+      // 防止对方 ajax({} as any) 强制绕过类型检查
       return;
     }
 
     const queryString = formatUrl(options.data);
-    const onStateChange = () => {// 状态判断
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState === 4) {
-                clearTimeout(timer);// 请求发送成功后其实可以清楚timer了
-                if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
-                    resolve(xhr.responseText);
-                } else {
-                    reject(xhr.status);
-                }
-            }
+    const onStateChange = () => {
+      // 状态判断
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          clearTimeout(timer); // 请求发送成功后其实可以清楚timer了
+          if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+            resolve(xhr.responseText);
+          } else {
+            reject(xhr.status);
+          }
         }
-    
-    }
+      };
+    };
     let timer;
     let xhr;
 
     if ((window as any).XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
+      // 兼容
+      xhr = new XMLHttpRequest();
     } else {
-        xhr = new ActiveXObject('Microsoft.XMLHTTP');// ie 兼容xhr的一种形式
+      xhr = new ActiveXObject("Microsoft.XMLHTTP"); // ie 兼容xhr的一种形式
     }
 
-    if (options.type.toUpperCase() === 'GET') {
-        xhr.open('GET', `${options.url}?${queryString}`);
-        onStateChange();
-        xhr.send();
-    } else if (options.type.toUpperCase() === 'POST') {
-        xhr.open('POST', options.url);
-        xhr.setRequestHeader('COntentType', 'application/x-www-form-urlencoded');
-        onStateChange();
-        xhr.send(options.data);
+    if (options.type.toUpperCase() === "GET") {
+      // 为啥还要这样？为了防止不规范使用 as any
+      xhr.open("GET", `${options.url}?${queryString}`);
+      onStateChange();
+      xhr.send();
+    } else if (options.type.toUpperCase() === "POST") {
+      xhr.open("POST", options.url);
+      xhr.setRequestHeader("COntentType", "application/x-www-form-urlencoded");
+      onStateChange();
+      xhr.send(options.data);
     }
-
 
     if (options.timeout) {
-        timer = setTimeout(()=> {
-            xhr.abort();
-            reject("timeout");
-        },options.timeout)
+      timer = setTimeout(() => {
+        xhr.abort();
+        reject("timeout");
+      }, options.timeout);
     }
 
     // 判断GET POST
     //xhr.send()
-  })
+  });
 }
