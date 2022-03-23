203d7c08626a2cdba0ed3da8c6b221b13b444c72
Cenkaifeng <cenkaifeng@foxmail.com>
2022-03-24 00:23:07 +0800

    mod: demoRender.js

--- fe_class/Vue源码/Reactive/Vue3.x/demoRender.js
+++ fe_class/Vue源码/Reactive/Vue3.x/demoRender.js
@@ -14,15 +14,22 @@ const vnode = {
   //...
 };
 
-function renderer(domString, container) {
-  container.innerHTML = domString;
-}
+// e.g.
+// function renderer(domString, container) {
+//   container.innerHTML = domString;
+// }
 
 function createRenderer(options) {
   // 通过 options 获得操作DOM 的API
 
-  const { createElement, insert, setElementText } = options;
+  const { createElement, insert, setElementText, patchProps } = options;
 
+  function shouldSetAsProps(el, key, value) {
+    // 特殊处理
+    if (key === "form" && el.tagName === "INPUT") return false;
+
+    return key in el;
+  }
   function mountElement(vnode, container) {
     // 创建 DOM
     const el = createElement(vnode.type);
@@ -40,22 +47,8 @@ function createRenderer(options) {
 
     if (vnode.props) {
       for (const key in vnode.props) {
-        // 用 in 判断 key 是否存在对应 DOM Properties
-        if (key in el) {
-          const type = typeof el[key];
-          const value = vnode.props[key];
-
-          // 如果是布尔类型，并且 value 是空字符串，则将值矫正为 true
-          if (type === "boolean" && value === "") {
-            el[key] = true;
-          } else {
-            el[key] = value;
-          }
-        } else {
-          // 如果要设置的属性没有对应 DOM Properties,则使用 setAttribute 函数设置属性
-          el.setElementText(key, vnode.props[key]);
-          // el[key] = vnode.props[key]; // 理解 HTML Attributes 和 DOM Properties 之间的差异和关联是合理设计虚拟节点结构和设置元素属性的关键
-        }
+        // const value = vnode.props[key];
+        patchProps(el, key, null, vnode.props[key]);
       }
     }
 
@@ -87,3 +80,34 @@ function createRenderer(options) {
     //hydrate (ssr 同构水合)
   };
 }
+
+// 调用
+const render = createRenderer({
+  createElement(tag) {
+    return document.createElement(tag);
+  },
+  setElementText(el, text) {
+    el.textContent = text;
+  },
+  insert(el, parent, anchor = null) {
+    parent.insertBefore(el, anchor);
+  },
+  patchProps(el, key, preValue, nextValue) {
+    // 用 in 判断 key 是否存在对应 DOM Properties
+    // if (key in el)
+    if (shouldSetAsProps(el, key, nextValue)) {
+      const type = typeof el[key];
+
+      // 如果是布尔类型，并且 value 是空字符串，则将值矫正为 true
+      if (type === "boolean" && nextValue === "") {
+        el[key] = true;
+      } else {
+        el[key] = nextValue;
+      }
+    } else {
+      // 如果要设置的属性没有对应 DOM Properties,则使用 setAttribute 函数设置属性
+      el.setElementText(key, nextValue);
+      // el[key] = vnode.props[key]; // 理解 HTML Attributes 和 DOM Properties 之间的差异和关联是合理设计虚拟节点结构和设置元素属性的关键
+    }
+  },
+});
