8a38b4e75cead33f0d6e0a5d25a992306d255384
Cenkaifeng <jervisc@ones.com>
2024-05-17 17:58:28 +0800

    chore: async-cache add commit

--- fe_class/Typescript/async-cache.ts
+++ fe_class/Typescript/async-cache.ts
@@ -1,4 +1,12 @@
 const cacheMap = new Map();
+/**
+ * 启用缓存装饰器
+ *
+ * @param target 目标对象
+ * @param name 目标对象的属性名
+ * @param descriptor 目标对象的属性描述符
+ * @returns 返回修改后的属性描述符
+ */
 export function EnableCache(target: any, name: string, descriptor: PropertyDescriptor) {
     const val = descriptor.value;
     descriptor.value = async function (...args: any) {
